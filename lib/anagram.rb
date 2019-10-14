bc618f3c080b46873d85e05cc7d1544deddb09d8
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-14 17:42:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,12 @@
 class Anagram
   
-  attr_accessor :word, :word_array
+  attr_accessor :word
   
   def initialize(word)
     @word = word
   end
   
-  def check_for_anagrams(word_array)
+  def match(word_array)
     anagrams = []
     sorted_letters = @word.split("").sort
     word_array.each do |words|
